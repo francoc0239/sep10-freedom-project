32c4c8d468564ff9b5ab21929bd2c2530695e7f8
franco <francoc16@nycstudents.net>
2026-04-29 09:32:14 -0400

    a

--- sep10-freedom-project-Code/vite.config.js
+++ sep10-freedom-project-Code/vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sep10-freedom-project/sep10-freedom-project-Code/',
+
 })
