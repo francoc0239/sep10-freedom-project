5b8731473fe96a71b7caadc05c2b4367ea1a7e18
franco <francoc16@nycstudents.net>
2026-04-30 09:47:11 -0400

    a

--- sep10-freedom-project-Code/src/App.jsx
+++ sep10-freedom-project-Code/src/App.jsx
@@ -3,13 +3,22 @@ import "./App.css"; // Your styles now have the final word
 
 import { useState } from "react";
 import { Caresol } from "./components/Apple";
-
+import { Nav } from "./components/Nav";
 
 function App() {
   return (
-    <div>
-      <Caresol />
-      <h1 className="apple">knkerfkenkefnkefnkefnmekm</h1>
+    <div classname="container-fluid">
+      <div className="row">
+        <div className="col-sm-12">
+          <h1 className="title">The Medical Industry</h1>
+          <Caresol />
+        </div>
+      </div>
+      <div className="row">
+        <div classname="col-sm-12">
+          <Nav />
+        </div>
+      </div>
     </div>
   );
 }
