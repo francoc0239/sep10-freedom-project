85122923d79e7ef72731ee8f5f7a92dde50ca170
franco <francoc16@nycstudents.net>
2026-04-30 09:50:29 -0400

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
+import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap loads first
+import "./App.css"; // Your styles now have the final word
+
 import { useState } from "react";
-import "./App.css";
 import { Caresol } from "./components/Apple";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Nav } from "./components/Nav";
 
 function App() {
   return (
-    <>
-    <body>
-      <Caresol />
-      <h1>knkerfkenkefnkefnkefnmekm</h1>
-      </body>
-    </>
+    <div classname="container-fluid">
+      <div className="row">
+        <div className="col-sm-12">
+          <h1 className="title">The Medical Industry</h1>
+          <Caresol />
+        </div>
+      </div>
+      <div className="row">
+        <div classname="col-sm-12">
+          <Nav />
+        </div>
+      </div>
+    </div>
   );
 }
 
