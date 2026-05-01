2b9ca569988e161fb3a993f532a2f90e566d9685
franco <francoc16@nycstudents.net>
2026-05-01 09:43:59 -0400

    add css and content.jsx

--- sep10-freedom-project-Code/src/components/Content.jsx
+++ sep10-freedom-project-Code/src/components/Content.jsx
@@ -3,9 +3,42 @@ import "../App.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function Content(){
-    return(
+function Content() {
+    return (
         <>
+
+            <h1 className="text-center"> The Medical Industry</h1>
+            <div class="top">
+            <div class="container">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="card mb-3">
+                                <img src="..." class="card-img-top" alt="..." />
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                                </div>
+                            </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="card mb-3">
+                                    <img src="..." class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
+
+
 
         </>
 
